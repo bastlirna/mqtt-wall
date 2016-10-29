216ac47ffcb330f8da6a9269d1a4234092d12f31
ah01 <horcicaa@gmail.com>
2016-10-29 05:05:40 +0200

    Prepare logic for alphabet sorting (#19)

--- src/js/ui.js
+++ src/js/ui.js
@@ -24,10 +24,10 @@ UI.toast = function (message, type = "info", persistent = false) {
 
 export class MessageLine {
 
-    constructor(topic, $parent){
+    constructor(topic){
         this.topic = topic;
         this.counter = 0;
-        this.$parent = $parent;
+        this.isNew = true;
         this.init();
     }
 
@@ -49,8 +49,6 @@ export class MessageLine {
             .appendTo(header);
 
         this.$payload = $("<p>").appendTo(this.$root);
-        
-        this.$root.appendTo(this.$parent);
     }
 
     set isRetained(value) {
@@ -61,8 +59,8 @@ export class MessageLine {
         this.$payload.toggleClass("sys", value);
     }
 
-    highlight() {
-        this.$payload
+    highlight(line = false) {
+        (line ? this.$root : this.$payload)
             .stop()
             .css({backgroundColor: "#0CB0FF"})
             .animate({backgroundColor: "#fff"}, 2000);
@@ -87,12 +85,18 @@ export class MessageLine {
         }
 
         this.$payload.text(payload);
-        this.highlight();       
+        this.highlight(this.isNew);
+
+        if (this.isNew) {
+            this.isNew = false;
+        }       
     }
 }
 
 export class MessageContainer {
+
     constructor($parent) {
+        this.sort = 'Alphabetically';
         this.$parent = $parent;
         this.init();
     }
@@ -103,16 +107,50 @@ export class MessageContainer {
 
     reset() {
         this.lines = {};
+        this.topics = [];
         this.$parent.html("");
     }
 
     update (topic, payload, retained) {
 
         if (!this.lines[topic]) {
-            this.lines[topic] = new MessageLine(topic, this.$parent);
+
+            var line = new MessageLine(topic, this.$parent);
+
+            this[`addLine${this.sort}`](line);
+            this.lines[topic] = line;
         }
 
         this.lines[topic].update(payload, retained);
+    }
+
+    addLineAlphabetically (line) {
+
+        if (this.topics.length == 0) 
+        {
+            this.addLineChronologically(line);
+            return;
+        }
+
+        var topic = line.topic;
+
+        this.topics.push(topic);
+        this.topics.sort();
+
+        var n = this.topics.indexOf(topic);
+
+        if (n == 0){
+            this.$parent.prepend(line.$root);
+            return;    
+        }
+
+        var prev = this.topics[n - 1];
+        line.$root.insertAfter(this.lines[prev].$root);
+    }
+
+    addLineChronologically (line) {
+        this.topics.push(line.topic);
+        this.$parent.append(line.$root);
     }
 }
 
